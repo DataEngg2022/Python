e6a6f6a79fc0eb2f963135c564879a303baa2ee5
DataEngg2022 <111466250+DataEngg2022@users.noreply.github.com>
2023-11-12 18:33:07 +1300

    Add files via upload

---
+++ lab003.py
@@ -0,0 +1,18 @@
+#print("test")
+
+#  Variables
+
+#print("Hello World", sep= ",")
+
+#print ("Hell0","Boss","Morning",sep=" * ",end ="\t",file="E:\ADE\ADF\PRODUCT.txt")
+
+#print("luck")
+
+
+file= open("E:\ADE\ADF\PRODUCT.txt","r")
+
+contents = file.read()
+
+print(contents)
+
+file.close()
