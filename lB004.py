fa6a2cc212f81b9130693565cd29fee5953f79a8
DataEngg2022 <111466250+DataEngg2022@users.noreply.github.com>
2023-11-12 18:57:44 +1300

    Add files via upload

---
+++ lB004.py
@@ -0,0 +1,18 @@
+# take input from user  num1 and num2
+
+# num1 + num2
+
+#numb1 - num2
+
+
+
+my_value1 = input("Enter value1\n")
+
+my_value2 = input("Enter value2\n")
+
+print( int(my_value1) + int(my_value2))
+
+
+my_value3 = "test01"
+
+print(my_value3[2])
